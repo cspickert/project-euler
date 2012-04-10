fb90e6653fa8f700f3b1bae4b2153df6d7a9c988
Cameron Spickert <cameron@aviary.com>
2012-04-10 16:54:38 -0400

    Added problem 6

---
+++ python/problem06.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+def sum_of_squares(n):
+    def squares(n):
+        for i in range(1, n + 1):
+            yield i ** 2
+    return sum(squares(n))
+
+def square_of_sum(n):
+    return sum(xrange(1, n + 1)) ** 2
+
+def problem6():
+    n = 100
+    return abs(sum_of_squares(n) - square_of_sum(n))
+
+if __name__ == "__main__":
+    print problem6()
