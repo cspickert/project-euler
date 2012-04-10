4f57c94925310a54a4f8a15ee5aec569d4bffa6a
Cameron Spickert <cspicker@gmail.com>
2012-04-10 01:22:52 -0300

    Renamed variable `sum` to `total` in problem 2

--- python/problem02.py
+++ python/problem02.py
@@ -3,16 +3,16 @@
 def problem2():
     limit = 4*10**6
     stack = [0, 1]
-    sum = 0
+    total = 0
     while True:
         n = stack[0] + stack[1]
         if n > limit:
             break
         if n % 2 == 0:
-            sum += n
+            total += n
         stack[0] = stack[1]
         stack[1] = n
-    return sum
+    return total
 
 if __name__ == "__main__":
     print problem2()
