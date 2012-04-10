71ce84419a7e53b8b5dd5042bc9460f709a7db9a
Cameron Spickert <cameron@aviary.com>
2012-04-10 00:19:00 -0400

    Fixed tabs

--- python/problem01.py
+++ python/problem01.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 def problem1():
-	return sum(filter(lambda i: i % 3 == 0 or i % 5 == 0, range(1000)))
+    return sum(filter(lambda i: i % 3 == 0 or i % 5 == 0, range(1000)))
 
 if __name__ == "__main__":
-	print problem1()
+    print problem1()
--- python/problem04.py
+++ python/problem04.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
 
 def reverse_str(s):
-	return "".join([c for c in reversed(s)])
+    return "".join([c for c in reversed(s)])
 
 def is_palindrome(n):
-	return str(n) == reverse_str(str(n))
+    return str(n) == reverse_str(str(n))
 
 def problem4():
-	palindromes = []
-	for i in xrange(10**2, 10**3):
-		for j in xrange(10**2, i + 1):
-			n = i * j
-			if is_palindrome(n):
-				palindromes.append(n)
-	return max(palindromes)
+    palindromes = []
+    for i in xrange(10**2, 10**3):
+        for j in xrange(10**2, i + 1):
+            n = i * j
+            if is_palindrome(n):
+                palindromes.append(n)
+    return max(palindromes)
 
 if __name__ == "__main__":
-	print problem4()
+    print problem4()
